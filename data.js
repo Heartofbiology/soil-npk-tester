6a94e9af86386cfa21e5ed67f8577223062b4ee4
Heartofbiology <heartofbiology22@gmail.com>
2026-03-24 23:39:27 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,50 @@
+const crops = [
+{
+    name: "Rice",
+    states: ["Punjab","Tamil Nadu"],
+    npk: [6,6,6],
+    ph: [5,7],
+    pests: [
+        {name:"Brown Hopper", solution:"Neem Oil Spray"}
+    ],
+    price: 2200,
+    trend: "High Demand",
+    image: "images/rice.jpg"
+},
+{
+    name: "Wheat",
+    states: ["Punjab","UP"],
+    npk: [5,4,4],
+    ph: [6,7.5],
+    pests: [
+        {name:"Aphids", solution:"Imidacloprid"}
+    ],
+    price: 2100,
+    trend: "Stable",
+    image: "images/wheat.jpg"
+},
+{
+    name: "Cotton",
+    states: ["Maharashtra","Punjab"],
+    npk: [5,5,6],
+    ph: [6,8],
+    pests: [
+        {name:"Bollworm", solution:"Bt Spray"}
+    ],
+    price: 6000,
+    trend: "Rising",
+    image: "images/cotton.jpg"
+},
+{
+    name: "Sugarcane",
+    states: ["UP","Tamil Nadu"],
+    npk: [7,5,6],
+    ph: [6,7.5],
+    pests: [
+        {name:"Stem Borer", solution:"Chlorpyrifos"}
+    ],
+    price: 3500,
+    trend: "Growing",
+    image: "images/sugarcane.jpg"
+}
+];
