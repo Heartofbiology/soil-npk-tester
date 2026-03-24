496d5d60c13c49ed14e0f3aaba6597d9c70809ec
Heartofbiology <heartofbiology22@gmail.com>
2026-03-24 23:57:50 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,50 +1,267 @@
 const crops = [
+
 {
-    name: "Rice",
-    states: ["Punjab","Tamil Nadu"],
-    npk: [6,6,6],
-    ph: [5,7],
-    pests: [
-        {name:"Brown Hopper", solution:"Neem Oil Spray"}
-    ],
-    price: 2200,
-    trend: "High Demand",
-    image: "images/rice.jpg"
+name:"Rice",
+states:["Punjab","Tamil Nadu","West Bengal"],
+npk:[6,6,6],
+ph:[5,7],
+price:{Punjab:2200,TamilNadu:2100,WB:2000},
+pests:["Brown Hopper","Stem Borer"],
+fertilizer:"Urea + DAP",
+image:"images/rice.jpg"
 },
+
 {
-    name: "Wheat",
-    states: ["Punjab","UP"],
-    npk: [5,4,4],
-    ph: [6,7.5],
-    pests: [
-        {name:"Aphids", solution:"Imidacloprid"}
-    ],
-    price: 2100,
-    trend: "Stable",
-    image: "images/wheat.jpg"
+name:"Wheat",
+states:["Punjab","UP","Haryana"],
+npk:[5,4,4],
+ph:[6,7.5],
+price:{Punjab:2100,UP:2050},
+pests:["Aphids","Termites"],
+fertilizer:"DAP + Potash",
+image:"images/wheat.jpg"
 },
+
 {
-    name: "Cotton",
-    states: ["Maharashtra","Punjab"],
-    npk: [5,5,6],
-    ph: [6,8],
-    pests: [
-        {name:"Bollworm", solution:"Bt Spray"}
-    ],
-    price: 6000,
-    trend: "Rising",
-    image: "images/cotton.jpg"
+name:"Maize",
+states:["Karnataka","Bihar","MP"],
+npk:[5,5,5],
+ph:[5.5,7],
+price:{Karnataka:1800,Bihar:1700},
+pests:["Armyworm"],
+fertilizer:"NPK Mix",
+image:"images/maize.jpg"
 },
+
 {
-    name: "Sugarcane",
-    states: ["UP","Tamil Nadu"],
-    npk: [7,5,6],
-    ph: [6,7.5],
-    pests: [
-        {name:"Stem Borer", solution:"Chlorpyrifos"}
-    ],
-    price: 3500,
-    trend: "Growing",
-    image: "images/sugarcane.jpg"
+name:"Cotton",
+states:["Maharashtra","Punjab","Gujarat"],
+npk:[5,5,6],
+ph:[6,8],
+price:{Maharashtra:6000,Gujarat:5800},
+pests:["Bollworm"],
+fertilizer:"Potash Rich",
+image:"images/cotton.jpg"
+},
+
+{
+name:"Sugarcane",
+states:["UP","Tamil Nadu","Maharashtra"],
+npk:[7,5,6],
+ph:[6,7.5],
+price:{UP:3500},
+pests:["Stem Borer"],
+fertilizer:"High Nitrogen",
+image:"images/sugarcane.jpg"
+},
+
+{
+name:"Tomato",
+states:["Karnataka","AP","TN"],
+npk:[5,6,5],
+ph:[5.5,7],
+price:{Karnataka:1500},
+pests:["Whitefly"],
+fertilizer:"DAP + Compost",
+image:"images/tomato.jpg"
+},
+
+{
+name:"Potato",
+states:["UP","Punjab","WB"],
+npk:[6,5,5],
+ph:[5,6.5],
+price:{UP:1400},
+pests:["Late Blight"],
+fertilizer:"Potash",
+image:"images/potato.jpg"
+},
+
+{
+name:"Onion",
+states:["Maharashtra","Karnataka"],
+npk:[5,5,6],
+ph:[6,7],
+price:{Maharashtra:1600},
+pests:["Thrips"],
+fertilizer:"Sulfur Mix",
+image:"images/onion.jpg"
+},
+
+{
+name:"Chilli",
+states:["AP","Telangana"],
+npk:[5,6,6],
+ph:[6,7],
+price:{AP:5000},
+pests:["Aphids"],
+fertilizer:"Organic Compost",
+image:"images/chilli.jpg"
+},
+
+{
+name:"Groundnut",
+states:["Gujarat","TN"],
+npk:[4,5,5],
+ph:[6,7.5],
+price:{Gujarat:5500},
+pests:["Leaf Miner"],
+fertilizer:"Gypsum",
+image:"images/groundnut.jpg"
+},
+
+{
+name:"Mustard",
+states:["Rajasthan","UP"],
+npk:[4,4,5],
+ph:[6,8],
+price:{Rajasthan:5000},
+pests:["Aphids"],
+fertilizer:"Nitrogen Mix",
+image:"images/mustard.jpg"
+},
+
+{
+name:"Soybean",
+states:["MP","Maharashtra"],
+npk:[5,5,5],
+ph:[6,7],
+price:{MP:4000},
+pests:["Caterpillar"],
+fertilizer:"DAP",
+image:"images/soybean.jpg"
+},
+
+{
+name:"Banana",
+states:["TN","Kerala"],
+npk:[7,6,7],
+ph:[6,7],
+price:{TN:3000},
+pests:["Nematodes"],
+fertilizer:"Organic + Potash",
+image:"images/banana.jpg"
+},
+
+{
+name:"Mango",
+states:["UP","AP"],
+npk:[5,5,6],
+ph:[5.5,7.5],
+price:{UP:6000},
+pests:["Fruit Fly"],
+fertilizer:"Organic Compost",
+image:"images/mango.jpg"
+},
+
+{
+name:"Grapes",
+states:["Maharashtra"],
+npk:[6,5,6],
+ph:[6,7],
+price:{Maharashtra:7000},
+pests:["Mealybugs"],
+fertilizer:"Potash Rich",
+image:"images/grapes.jpg"
+},
+
+{
+name:"Apple",
+states:["Himachal"],
+npk:[5,5,5],
+ph:[5.5,6.5],
+price:{Himachal:8000},
+pests:["Scab"],
+fertilizer:"Organic",
+image:"images/apple.jpg"
+},
+
+{
+name:"Turmeric",
+states:["Telangana","TN"],
+npk:[6,5,6],
+ph:[5.5,7],
+price:{Telangana:9000},
+pests:["Rhizome Rot"],
+fertilizer:"Compost",
+image:"images/turmeric.jpg"
+},
+
+{
+name:"Ginger",
+states:["Kerala"],
+npk:[6,5,5],
+ph:[5.5,6.5],
+price:{Kerala:8500},
+pests:["Soft Rot"],
+fertilizer:"Organic",
+image:"images/ginger.jpg"
+},
+
+{
+name:"Cabbage",
+states:["UP","WB"],
+npk:[5,5,5],
+ph:[6,7],
+price:{UP:1200},
+pests:["Caterpillar"],
+fertilizer:"DAP",
+image:"images/cabbage.jpg"
+},
+
+{
+name:"Carrot",
+states:["Punjab","UP"],
+npk:[5,4,5],
+ph:[6,7],
+price:{Punjab:1300},
+pests:["Root Fly"],
+fertilizer:"Potash",
+image:"images/carrot.jpg"
+},
+
+{
+name:"Pea",
+states:["UP"],
+npk:[4,5,5],
+ph:[6,7],
+price:{UP:2500},
+pests:["Aphids"],
+fertilizer:"DAP",
+image:"images/pea.jpg"
+},
+
+{
+name:"Sunflower",
+states:["Karnataka"],
+npk:[5,5,6],
+ph:[6,7],
+price:{Karnataka:4000},
+pests:["Moth"],
+fertilizer:"Nitrogen",
+image:"images/sunflower.jpg"
+},
+
+{
+name:"Tea",
+states:["Assam"],
+npk:[6,5,5],
+ph:[4.5,5.5],
+price:{Assam:10000},
+pests:["Looper"],
+fertilizer:"Acidic Soil Mix",
+image:"images/tea.jpg"
+},
+
+{
+name:"Coffee",
+states:["Karnataka"],
+npk:[6,5,5],
+ph:[5.5,6.5],
+price:{Karnataka:12000},
+pests:["Berry Borer"],
+fertilizer:"Organic",
+image:"images/coffee.jpg"
 }
+
 ];
